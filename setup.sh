fdae137a3d65d788e6e0d5473f312aebd9d8a0af
TuinderJ <joshua.tuinder@gmail.com>
2024-10-17 05:55:22 -0600

    add tmux.conf file

--- setup.sh
+++ setup.sh
@@ -9,17 +9,17 @@ cd ~
 git clone https://github.com/TuinderJ/arch-install.git
 cd arch-install
 
+# NVIM
+
 # TMUX
 if [ ! -e /bin/tmux ]
 then
   sudo pacman -S tmux
 fi
 
-if [ -e ~/.tmux.conf ]
+if [ ! -e ~/.tmux.conf ]
 then
-  echo "TMux config file exists"
-else
-  echo "TMux config does not exist"
+  cp .dotfiles/.tmux.conf ~/.tmux.conf
 fi
 
 # Clean up this repo on the local machine
